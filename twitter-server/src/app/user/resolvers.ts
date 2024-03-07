dbd6f82b1292f6bb43a62fe3581f94129be1e58b
Avadhut Lohar <loharavdhoot@gmail.com>
2024-03-07 23:16:01 +0530

    Verify  JWT token

--- twitter-server/src/app/user/resolvers.ts
+++ twitter-server/src/app/user/resolvers.ts
@@ -1,6 +1,56 @@
+import axios from "axios";
+import { response } from "express";
+import { prismaClient } from "../../clients/db";
+
+interface GoogleTokenResult{
+  iss?: string;
+  nbf?: string;
+  aud?: string;
+  sub?: string;
+  email?: string;
+  email_verified?: string;
+  azp?: string;
+  name?: string;
+  picture?: string;
+  given_name?: string;
+  family_name?: string;
+  iat?: string;
+  exp?: string;
+  jti?: string;
+  alg?: string;
+  kid?: string;
+  typ?: string;
+}
+
 const queries = {
   verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
-    return token;
+    const googleToken =token;
+
+    const googleOauthURL = new URL("https://oauth2.googleapis.com/tokeninfo")
+
+    googleOauthURL.searchParams.set("id_token", googleToken);
+
+    const { data } = await axios.get<GoogleTokenResult>(googleOauthURL.toString(),{responseType: "json"});
+
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          firstName : data.given_name,
+          lastName: data.family_name,
+          email : data.email,
+          profileImageURL: data.picture,
+        },
+      });
+    }
+
+
+    return data.email;
   },
 };
 export const resolvers = {  queries
